f11103d20c8027c25f9fdfa6790049085bb98cd1
Marcos <marcosbadillo089@gmail.com>
2021-04-25 18:10:53 -0500

    add functions in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,38 @@
 //--------------------global Variables----------------------//
-userInput = '';
+var tasks = $('.description');
+var savedTasks = JSON.parse(localStorage.getItem("savedTasks")) || [];
 var saveButton = document.getElementById('saveBtn');
 var deleteButton = document.getElementById('delBtn');
 var addToDo = document.getElementById('addToDo');
 
 //-----------------End of global Variables------------------//
+
+//----------------------Time Function-----------------------//
+function currentTime() {
+    var currentDate = moment();
+  
+    $("#currentDay").html(currentDate.format("dddd, MMMM DD"));
+    
+    var currentHour = currentDate.hours();
+    
+    $(".description").addClass("future");
+    
+    if(currentHour > 17){
+      $(".description").removeClass("future");
+      $(".description").addClass("past");
+    } else {
+    for(var i = currentHour - 1; i >= 9; i--){
+      $(".description").eq(i - 9).removeClass("future");
+      $(".description").eq(i - 9).addClass("past");
+      }
+      $(".description").eq(currentHour - 9).removeClass("future");
+      $(".description").eq(currentHour - 9).addClass("present");
+    }
+    loadTasks();
+  };
+  
+  currentTime();
+//-----------------End of Time Function---------------------//
 
 
 //--------------------addingToDo Function-------------------//
@@ -17,6 +45,7 @@ function addingToDo() {
     else {
         alert('You added to the ToDo list!')
 }
+saveToDo();
 }
 //----------------End of addingToDo Function----------------//
 
@@ -29,13 +58,19 @@ function deleteToDo() {
 
 
 //---------------------saveToDo Function--------------------//
-function saveToDo() {
-addingToDo()
-    saveButton = localStorage.setItem('')
+function saveToDo(index) {
+
+    savedTasks[index] = tasks[index].value;
+    localStorage.setItem("savedTasks", JSON.stringify(savedTasks));
+
 
 }
 //------------------End of saveToDo function-----------------//
-
+function loadTasks() {
+for(var i = 0; i < savedTasks.length; i++) {
+    tasks[i].value = savedTasks[i];
+    }
+}
 
 //-------------------addEventLisnter Section-----------------//
 document.getElementById('delBtn').addEventListener('click', deleteToDo);
